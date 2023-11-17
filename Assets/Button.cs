c1a422dd90f44e278ef486c31106beea0d6bd0e0
jcpiazza <144396675+jcpiazza@users.noreply.github.com>
2023-11-17 14:35:01 -0500

    button fixed

---
+++ Assets/Button.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMod1Scene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+
+}
+
